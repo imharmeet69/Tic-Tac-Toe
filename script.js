82b80dc969fc2436e1d6fbd49ceb7f75abaad2d0
Harmeet Singh <164763075+imharmeet69@users.noreply.github.com>
2024-11-09 19:37:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+var playTurn, move, isGameOver, span, restartBtn;
+playTurn = "X";
+move = 0;
+isGameOver = false;
+span = document.getElementsByTagName("span");
+restartBtn = '<button onclick="playAgain()"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16"><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/></svg></button>';
+
+function play(y) {
+  if (y.dataset.player == "none" && isGameOver == false) {
+    y.innerHTML = playTurn;
+    y.dataset.player = playTurn;
+    move++;
+    playTurn = (playTurn === "X") ? "O" : "X";
+  }
+
+  checkWinner(1, 2, 3);
+  checkWinner(4, 5, 6);
+  checkWinner(7, 8, 9);
+  checkWinner(1, 4, 7);
+  checkWinner(2, 5, 8);
+  checkWinner(3, 6, 9);
+  checkWinner(1, 5, 9);
+  checkWinner(3, 5, 7);
+
+  if (move == 9 && isGameOver == false) {
+    draw();
+  }
+}
+
+function checkWinner(a, b, c) {
+  a--;
+  b--;
+  c--;
+  if ((span[a].dataset.player === span[b].dataset.player && span[b].dataset.player === span[c].dataset.player && span[a].dataset.player !== "none") && isGameOver === false) {
+    span[a].parentNode.classList.add("activeBox");
+    span[b].parentNode.classList.add("activeBox");
+    span[c].parentNode.classList.add("activeBox");
+    gameOver(a);
+  }
+}
+
+function playAgain() {
+  document.getElementsByClassName("alert")[0].remove();
+  resetGame();
+  isGameOver = false;
+  for (var k = 0; k < span.length; k++) {
+    span[k].parentNode.classList.remove("activeBox");
+  }
+}
+
+function resetGame() {
+  for (let i = 0; i < span.length; i++) {
+    span[i].dataset.player = "none";
+    span[i].innerHTML = "";
+  }
+  playTurn = "X";
+}
+
+function gameOver(a) {
+  var gameOverAlertElement = "<b>GAME OVER</b><br><br> Player " + span[a].dataset.player + " Wins!!! <br><br>" + restartBtn;
+  var div = document.createElement("div");
+  div.className = "alert";
+  div.innerHTML = gameOverAlertElement;
+  document.body.appendChild(div);
+  isGameOver = true;
+  move = 0;
+}
+
+function draw() {
+  var drawAlertElement = '<b>DRAW !!!</b>' + restartBtn;
+  var div = document.createElement("div");
+  div.className = "alert";
+  div.innerHTML = drawAlertElement;
+  document.body.appendChild(div);
+  isGameOver = true;
+  move = 0;
+}
